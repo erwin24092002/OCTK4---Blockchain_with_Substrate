92689da354dc5b10c02f2baf6dc592375f3f265d
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-10-20 10:43:00 +0700

    update lot

---
+++ CLASS01 - Basic of Rust/assignment01/target/debug/Assignment01.d
@@ -0,0 +1,1 @@
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS01\ -\ Basic\ of\ Rust\Assignment01\target\debug\Assignment01.exe: C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS01\ -\ Basic\ of\ Rust\Assignment01\src\main.rs
---
+++ CLASS01 - Basic of Rust/assignment01/target/debug/deps/Assignment01.d
@@ -0,0 +1,5 @@
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS01 - Basic of Rust\Assignment01\target\debug\deps\Assignment01.exe: src\main.rs
+
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS01 - Basic of Rust\Assignment01\target\debug\deps\Assignment01.d: src\main.rs
+
+src\main.rs:
