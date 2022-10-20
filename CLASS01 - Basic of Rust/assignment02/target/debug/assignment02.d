b93727b5997e2b1949309bc2d8323e4b63d70f56
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-10-20 12:01:12 +0700

    upload assignment2

---
+++ CLASS01 - Basic of Rust/assignment02/target/debug/assignment02.d
@@ -0,0 +1,1 @@
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS01\ -\ Basic\ of\ Rust\assignment02\target\debug\assignment02.exe: C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS01\ -\ Basic\ of\ Rust\assignment02\src\main.rs
