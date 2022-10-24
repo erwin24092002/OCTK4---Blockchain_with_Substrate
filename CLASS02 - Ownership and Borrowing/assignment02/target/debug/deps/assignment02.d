05839af5abe823a7780b371b0ac64e75035e54d2
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-10-24 09:53:24 +0700

    upload assignment02

---
+++ CLASS02 - Ownership and Borrowing/assignment02/target/debug/deps/assignment02.d
@@ -0,0 +1,5 @@
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS02 - Ownership and Borrowing\assignment02\target\debug\deps\assignment02.exe: src\main.rs
+
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS02 - Ownership and Borrowing\assignment02\target\debug\deps\assignment02.d: src\main.rs
+
+src\main.rs:
