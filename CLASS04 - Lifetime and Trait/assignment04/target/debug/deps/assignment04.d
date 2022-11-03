cbc22f6912c983c2bde45b37561a80e90746bc07
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-11-03 15:11:37 +0700

    upload assignment4

---
+++ CLASS04 - Lifetime and Trait/assignment04/target/debug/deps/assignment04.d
@@ -0,0 +1,5 @@
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS04 - Lifetime and Trait\assignment04\target\debug\deps\assignment04.exe: src\main.rs
+
+C:\Users\20521\OneDrive\Documents\GitHub\OCTK4---Blockchain_with_Substrate\CLASS04 - Lifetime and Trait\assignment04\target\debug\deps\assignment04.d: src\main.rs
+
+src\main.rs:
