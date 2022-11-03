bb77c92c96a7c66a0677264fdc70cb65071933b1
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-11-03 13:03:08 +0700

    update assignment1

--- CLASS04 - Lifetime and Trait/assignment01/src/main.rs
+++ CLASS04 - Lifetime and Trait/assignment01/src/main.rs
@@ -3,10 +3,12 @@
 // Sửa code để compile thành công liên quan tới vấn đề lifetime
 // ///////////////////////////////////
 
+#![allow(unused)]
 use std::io;
 fn main() {
-    let mut input: Vec<&str>;
+    // let mut input: Vec<&str>;
     loop {
+        let mut input: Vec<&str>;
         let mut input_text = String::new();
         println!("Type instruction in the format Add <name> to <department>:");
         io::stdin().read_line(&mut input_text).expect("failed to read from stdin");
@@ -18,5 +20,5 @@ fn main() {
             println!("Invalid format.");
         }
     }
-    println!("{:?}", input);
+    // println!("{:?}", input);
 }
